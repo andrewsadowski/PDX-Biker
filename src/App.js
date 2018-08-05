328ea067f77cc716be888fed46a395f10cca1ea2
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-08-05 14:02:15 -0700

    added Header component and imported it into App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import LeafletMap from './components/LeafletMap';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <LeafletMap />
       </div>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h2>PDX Biker</h2>
+        <p>Find the closest bike-friendly streets near you</p>
+      </div>
+    );
+  }
+}
