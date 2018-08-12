f46eb6de14fb6be1f0c248cce58b66bc3358fa7b
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-08-11 17:48:57 -0700

    added axios and url for geojson data request, save geojson to state

--- src/components/CurrentLocLeafletMap.js
+++ src/components/CurrentLocLeafletMap.js
@@ -1,6 +1,13 @@
 import React, { createRef, Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import '../assets/Recommended_Bicycle_Routes.geojson';
+import {
+  Map,
+  TileLayer,
+  Marker,
+  Popup,
+  GeoJSON
+} from 'react-leaflet';
+import axios from 'axios';
+import geoJsonData from '../assets/Recommended_Bicycle_Routes.geojson';
 import './EventedLeafletMap.css';
 
 export default class CurrentLocLeafletMap extends Component {
@@ -9,10 +16,24 @@ export default class CurrentLocLeafletMap extends Component {
     latlng: {
       lat: 45.5127,
       lng: -122.679565
-    }
+    },
+    geoJSON: []
   };
 
   mapRef = createRef();
+
+  componentDidMount() {
+    this.mapRef.current.leafletElement.locate({ setView: true });
+    axios
+      .get(
+        'https://opendata.arcgis.com/datasets/40151125cedd49f09d211b48bb33f081_183.geojson'
+      )
+      .then(data => {
+        const geoJSONData = data.data.features;
+        this.setState({ geoJSON: geoJSONData });
+        console.log(data, geoJSONData);
+      });
+  }
 
   handleClick = () => {
     console.log(this.mapRef.current.leafletElement);
@@ -20,6 +41,7 @@ export default class CurrentLocLeafletMap extends Component {
   };
 
   handleLocationFound = e => {
+    console.log(e);
     this.setState({
       hasLocation: true,
       latlng: e.latlng
@@ -51,6 +73,9 @@ export default class CurrentLocLeafletMap extends Component {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {marker}
+        <GeoJSON
+          data={this.state.geoJSON ? this.state.geoJSON : null}
+        />
       </Map>
     );
   }
