a5b853b19441330355701068f01504ac13ea3b1d
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-08-15 18:28:53 -0700

    added Legend component to component directory

---
+++ src/components/Legend.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Legend:</h1>
+        <p />
+        <p />
+      </div>
+    );
+  }
+}
