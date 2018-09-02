a88e99778805f3677796642650361d76420d2ba7
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-01 21:54:09 -0700

    added styling for overlay header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,14 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header-container">
-        <h2 className="header-title">PDX Biker</h2>
+        <h2 className="header-title">
+          <span className="header-city">PDX </span>
+          <br />
+          Biker
+        </h2>
         <p className="header-subtitle">
-          Find the closest bike-friendly streets near you
+          Find the closest bike-friendly
+          <br /> streets near you
         </p>
       </div>
     );
