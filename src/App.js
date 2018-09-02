f5b5949a28f4b9ccd00fbc7b1ce53d721931d4ed
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-01 20:12:45 -0700

    removed legend component and associated css and removed import from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Map from './components/Map';
 import Header from './components/Header';
-import Legend from './components/Legend';
 import './App.css';
 
 class App extends Component {
