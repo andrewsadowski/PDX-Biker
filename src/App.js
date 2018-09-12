729eba5e9c6d75d2024723051e3cb925ac762a5e
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-11 18:47:01 -0700

    removed commented out watermark method, eslint/prettier updates

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Header/>
-                <MapContainer/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MapContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,119 +1,101 @@
 import React, { createRef, Component } from 'react';
 import ReactDOM from 'react-dom';
 import L from 'leaflet';
-import { Map, TileLayer, Marker, Popup, DivOverlay } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import './Map.css';
 
-const ARCGIS_REQUEST_URL = 'https://opendata.arcgis.com/datasets/40151125cedd49f09d211b48bb33f081_183.geojson';
+const ARCGIS_REQUEST_URL =
+  'https://opendata.arcgis.com/datasets/40151125cedd49f09d211b48bb33f081_183.geojson';
 
 export default class PDXMap extends Component {
-    constructor(props) {
-        super(props);
-        this.mapRef = createRef();
+  constructor(props) {
+    super(props);
+    this.mapRef = createRef();
+  }
+
+  componentDidMount() {
+    const { geoJSON, status } = this.props;
+    const map = this.mapRef.current.leafletElement;
+
+    /***
+     * Using navigator.geolocation.getCurrentPosition
+     * in MapContainer to get users position, which is
+     * then passed to Map component as props.
+     */
+    if (!status.locationFound) {
+      map.locate({ setView: true });
     }
 
-    componentDidMount() {
-        const { geoJSON, status } = this.props;
-        const map = this.mapRef.current.leafletElement;
-
-        /***
-         * Using navigator.geolocation.getCurrentPosition
-         * in MapContainer to get users position, which is
-         * then passed to Map component as props.
-         */
-        if (!status.locationFound) {
-            map.locate({ setView: true });
-        }
-
-        L.geoJSON(geoJSON, {
-            style: feature => {
-                return {
-                    weight: 5,
-                    opacity: 0.8
-                };
-            }
-        }).addTo(map);
-    }
-
-    handleClick = () => {
-        console.log('### this.mapRef:', this.mapRef)
-        this.mapRef.current.leafletElement.locate();
-    };
-
-    handleLocationFound = e => {
-        console.log(e);
-        this.setState({
-            hasLocation: true,
-            latlng: e.latlng
-        });
-    };
-
-    getGeoJsonStyle = (feature, layer) => {
+    L.geoJSON(geoJSON, {
+      style: feature => {
         return {
-            color: '#006400',
-            weight: 10,
-            opacity: 0.5
+          weight: 5,
+          opacity: 0.8
         };
+      }
+    }).addTo(map);
+  }
+
+  handleClick = () => {
+    console.log('### this.mapRef:', this.mapRef);
+    this.mapRef.current.leafletElement.locate();
+  };
+
+  handleLocationFound = e => {
+    console.log(e);
+    this.setState({
+      hasLocation: true,
+      latlng: e.latlng
+    });
+  };
+
+  getGeoJsonStyle = (feature, layer) => {
+    return {
+      color: '#006400',
+      weight: 10,
+      opacity: 0.5
     };
+  };
 
-    // addWatermark = () => {
-    //   const map = this.mapRef.current.leafletElement;
-    //   L.Control.Watermark = L.Control.extend({
-    //     onAdd: function(map) {
-    //       var img = L.DomUtil.create('img');
+  renderMarker() {
+    const { latlng } = this.props;
 
-    //       img.src = 'https://leafletjs.com/docs/images/logo.png';
-    //       img.style.width = '200px';
+    return (
+      <Marker position={latlng}>
+        <Popup>
+          <span>You are here!</span>
+        </Popup>
+      </Marker>
+    );
+  }
 
-    //       return img;
-    //     }
-    //   });
+  render() {
+    const { status, latlng, handleLocationFound } = this.props;
 
-    //   L.control.watermark = function(opts) {
-    //     return new L.Control.Watermark(opts);
-    //   };
+    return (
+      <Map
+        className="map-element"
+        center={latlng}
+        length={4}
+        onClick={this.handleClick}
+        setView={true}
+        onLocationfound={() => handleLocationFound()}
+        ref={this.mapRef}
+        zoom={14}
+      >
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-    //   L.control.watermark({ position: 'bottomleft' }).addTo(map);
-    // };
-
-    renderMarker() {
-        const {latlng} = this.props;
-
-        return (
-            <Marker position={latlng}>
-                <Popup>
-                    <span>You are here!</span>
-                </Popup>
-            </Marker>
-        );
-    }
-
-    render() {
-        const {status, latlng, handleLocationFound} = this.props;
-
-        return (
-            <Map
-                className="map-element"
-                center={latlng}
-                length={4}
-                onClick={this.handleClick}
-                setView={true}
-                onLocationfound={() => handleLocationFound()}
-                ref={this.mapRef}
-                zoom={14}
-            >
-                <TileLayer
-                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-
-                {status.locationFound && this.renderMarker()}
-            </Map>
-        );
-    }
+        {status.locationFound && this.renderMarker()}
+      </Map>
+    );
+  }
 }
 
 /**
- * TODO:  Add Header + Legend to
+ * TODO:  Remove Leaflet based GeoLocation management
  *
  */
