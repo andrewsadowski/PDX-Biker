5e293144f8a941cf11a14f7c9d156eacdad4ce37
admin <andrew.n.sadowski@gmail.com>
2018-09-14 13:53:55 -0700

    added React-GA import and constructor with ReactGA initialization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
-import MapContainer from './components/MapContainer';
-import Header from './components/Header';
-import './App.css';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import MapContainer from "./components/MapContainer";
+import Header from "./components/Header";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    //ReactGA.initialize('UA-125827074-1')
+    //ReactGA.pageview(window.location.pathname)
+  }
+
   render() {
     return (
       <div className="App">
