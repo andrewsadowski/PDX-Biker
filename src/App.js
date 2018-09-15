7145184b6e66d7d59a4c5ea6edd2fb2ffb08f813
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-14 19:09:12 -0700

    fixed eslint errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import ReactGA from "react-ga";
-import MapContainer from "./components/MapContainer";
-import Header from "./components/Header";
-import "./App.css";
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+import MapContainer from './components/MapContainer';
+import Header from './components/Header';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    ReactGA.initialize('UA-125827074-1')
-    ReactGA.pageview(window.location.pathname)
+    ReactGA.initialize('UA-125827074-1');
+    ReactGA.pageview(window.location.pathname);
   }
 
   render() {
